113016c3b82b3410403ef1e9e8c32bb6a2387508
Vedant <122482472+vedantparmar12@users.noreply.github.com>
2024-06-02 12:55:24 +0530

    Add files via upload

---
+++ selenium_automation/main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+service=Service(executable_path="chromedriver.exe")
+driver=webdriver.Chrome(service=service)
+
+driver.get("https://www.google.com")
+input_element=driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("youtube" + Keys.ENTER)
+
+time.sleep(7)
+
+driver.quit()
